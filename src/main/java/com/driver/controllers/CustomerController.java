c1b15480f610f65b84d603f0bad6ddbbcfec30ed
Kiranchandu Saragadam <110031953+kiranchandusaragadam@users.noreply.github.com>
2023-11-19 11:56:10 +0530

    9 update

--- src/main/java/com/driver/controllers/CustomerController.java
+++ src/main/java/com/driver/controllers/CustomerController.java
@@ -27,9 +27,15 @@ public class CustomerController {
 	}
 
 	@PostMapping("/bookTrip")
-	public ResponseEntity<Integer> bookTrip(@RequestParam Integer customerId, @RequestParam String fromLocation, @RequestParam String toLocation, @RequestParam Integer distanceInKm) throws Exception {
-		TripBooking bookedTrip = customerService.bookTrip(customerId, fromLocation, toLocation, distanceInKm);
-		return new ResponseEntity<>(bookedTrip.getTripBookingId(), HttpStatus.CREATED);
+	public ResponseEntity<?> bookTrip(@RequestParam Integer customerId, @RequestParam String fromLocation, @RequestParam String toLocation, @RequestParam Integer distanceInKm) throws Exception {
+		try{
+			TripBooking bookedTrip = customerService.bookTrip(customerId, fromLocation, toLocation, distanceInKm);
+			return new ResponseEntity<>(bookedTrip.getTripBookingId(), HttpStatus.CREATED);
+		}
+		catch (Exception e){
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+
 	}
 
 	@DeleteMapping("/complete")
